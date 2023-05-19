58f58b304ee666774a6fbf385d290a28e4073602
technologicaljerry <cserajatverma@gmail.com>
2023-05-19 23:41:32 +0530

    Update app.resolver.ts

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -1,1 +1,11 @@
-export class AppResolver {}
+/* eslint-disable prettier/prettier */
+import { Query, Resolver } from "@nestjs/graphql";
+
+@Resolver(() => String)
+export class AppResolver {
+
+    @Query(() => String)
+    index(): string {
+        return 'Welcome to NestJs GraphQL PostgresDB SERVER!'
+    }
+}
