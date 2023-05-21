a80b2f8110e6bfaa56ee782a452169f4e067f69d
technologicaljerry <cserajatverma@gmail.com>
2023-05-21 22:35:31 +0530

    add user

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -4,37 +4,10 @@ import { AppService } from "./app.service";
 
 @Resolver(() => String)
 export class AppResolver {
-    
-    constructor(private readonly usersService: AppService) { }
 
     @Query(() => String)
     index(): string {
         return 'Welcome to NestJs GraphQL PostgresDB SERVER!'
     }
 
-
-    @Mutation(() => User)
-    createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
-        return this.usersService.create(createUserInput);
-    }
-
-    @Query(() => [User], { name: 'users' })
-    findAll() {
-        return this.usersService.findAll();
-    }
-
-    @Query(() => User, { name: 'user' })
-    findOne(@Args('userId', { type: () => String }) userId: string) {
-        return this.usersService.findOne(userId);
-    }
-
-    @Mutation(() => User)
-    updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-        return this.usersService.update(updateUserInput.userId, updateUserInput);
-    }
-
-    @Mutation(() => User)
-    removeUser(@Args('userId', { type: () => String }) userId: string) {
-        return this.usersService.remove(userId);
-    }
 }
